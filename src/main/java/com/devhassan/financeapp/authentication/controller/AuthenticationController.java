35aca0afaa654719f5e03d3d0eea5b4b9a75b93a
Hassan <hassanb.2jz@gmail.com>
2023-10-24 18:09:07 +0300

    [commit 18 - server][AuthController error handling]

--- src/main/java/com/devhassan/financeapp/authentication/controller/AuthenticationController.java
+++ src/main/java/com/devhassan/financeapp/authentication/controller/AuthenticationController.java
@@ -4,7 +4,9 @@ import com.devhassan.financeapp.authentication.model.AuthenticationRequest;
 import com.devhassan.financeapp.authentication.model.AuthenticationResponse;
 import com.devhassan.financeapp.authentication.service.AuthenticationService;
 import com.devhassan.financeapp.authentication.model.RegisterRequest;
+import com.devhassan.financeapp.exceptions.DuplicateDataException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +20,16 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        try {
+            return ResponseEntity.ok(authenticationService.register(registerRequest));
+        } catch (DuplicateDataException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authRequest) {
         return ResponseEntity.ok(authenticationService.authenticate(authRequest));
-
     }
 }
