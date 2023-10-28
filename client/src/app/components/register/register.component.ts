400c762649112122fb4f09289d70e73f59b397e8
Hassan <hassanb.2jz@gmail.com>
2023-10-28 04:33:19 +0300

    [commit 17 - client][Successfully implemented registration component with validation]

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserRegisterRequest } from 'src/app/types/user-register-req';
+import Validation from 'src/app/types/validation';
 
 @Component({
   selector: 'app-register',
@@ -9,31 +12,78 @@ import { UserRegisterRequest } from 'src/app/types/user-register-req';
 })
 export class RegisterComponent implements OnInit {
 
-  form!: UserRegisterRequest;
-  isSuccessful = false;
-  isSignUpFailed = false;
-  errorMessage = '';
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+  submitted = false;
 
-
-  constructor(private authService: AuthService) { }
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
 
   onSubmit(): void {
-    const userRegister = this.form;
+    this.submitted = true;
 
-    this.authService.register(userRegister).subscribe({
-      next: data => {
-        console.log(data);
-        this.isSuccessful = true;
-        this.isSignUpFailed = false;
-      },
-      error: err => {
-        this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  regUser(): void {
+    if (this.submitted && !this.form.invalid) {
+      const userRegister: UserRegisterRequest = {
+        firstName: this.form.value.firstName,
+        lastName: this.form.value.lastName,
+        email: this.form.value.email,
+        password: this.form.value.password
       }
-    });
+
+      this.authService.register(userRegister).subscribe({
+        next: res => {
+          this.router.navigate(['/login']);
+        },
+        error: err => {
+          console.log(userRegister);
+          console.log(err);
+        }
+      });
+    }
   }
 }
